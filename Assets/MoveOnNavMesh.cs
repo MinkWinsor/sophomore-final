868da83ddd2f0f286c3c1fcc5c6c066f5b306df8
MinkWinsor <steven.olsen18@gmail.com>
2016-11-14 23:59:58 -0700

    Project Start
    Starting up the unity project.

---
+++ Assets/MoveOnNavMesh.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveOnNavMesh : MonoBehaviour {
+
+    public Transform target;
+    NavMeshAgent agent;
+
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.destination = target.position;
+    }
+
+    public void setNewTarget (Transform newTarget)
+    {
+        agent.destination = newTarget.position;
+    }
+}
