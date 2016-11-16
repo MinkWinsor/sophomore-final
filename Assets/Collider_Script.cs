e158a25dd94caecdb7ccba4431fc048e09b77dc0
MinkWinsor <steven.olsen18@gmail.com>
2016-11-16 13:16:37 -0700

    Figuring stuff out.
    Seems my idea doesn't quite work.

---
+++ Assets/Collider_Script.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Collider_Script : MonoBehaviour {
+
+    public Transform targetPosition;
+
+	// Use this for initialization
+	void Start () {
+        //var targetPosition = targetTransform.position;
+        //targetPosition.y = transform.position.y;
+        transform.LookAt(targetPosition);
+    }
+	
+
+    public void lookAt(Vector3 newTarget)
+    {
+        transform.LookAt(newTarget);
+    }
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Mouse_Position_Calculation.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mouse_Position_Calculation : MonoBehaviour {
+
+    public Transform LowerLeft;
+    public Transform LowerRight;
+    public Transform UpperLeft;
+    public Transform UpperRight;
+    public Transform moveCube;
+    public Collider_Script myCScript;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void OnMouseDown() {
+        myCScript.lookAt(Input.mousePosition);
+
+        //Debug.Log("Mouse Clicked: " + Input.mousePosition);
+        float _xRelativePosition = Input.mousePosition.x / Screen.width;
+        float _zRelativePosition = Input.mousePosition.y / Screen.height;
+        //Debug.Log("Relative Position: " + _xRelativePosition + _zRelativePosition);
+
+
+        
+        float _topAverage = (UpperRight.transform.position.x * _xRelativePosition) + (UpperLeft.transform.position.x * (1 - _xRelativePosition));
+        float _bottomAverage = (LowerRight.transform.position.x * _xRelativePosition) + (LowerLeft.transform.position.x * (1 - _xRelativePosition));
+        //Debug.Log(UpperLeft.transform.position.z + LowerLeft.transform.position.z);
+        Vector3 _middleAverage = new Vector3(
+            ((_topAverage * _zRelativePosition) + (_bottomAverage * (1 - _zRelativePosition))),
+            (0),
+
+            (((UpperLeft.transform.position.z * _zRelativePosition) + (LowerLeft.transform.position.z * (1 - _zRelativePosition))))
+            );
+        
+
+        moveCube.position = _middleAverage;
+
+
+    }
+
+
+    /*(
+            (((UpperLeft.transform.position.z * _zRelativePosition) + (LowerLeft.transform.position.z * (1 - _zRelativePosition)))
+            + ((UpperLeft.transform.position.z + LowerLeft.transform.position.z) / 2)*/
+}
