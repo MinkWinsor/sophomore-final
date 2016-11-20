df230f91d08b07a0124bbd2886936fda84875dd2
MinkWinsor <steven.olsen18@gmail.com>
2016-11-20 15:05:17 -0700

    Documentation of Code.
    Committing now because some changes were made to actual code, and then reversed.

--- Assets/Scripts/Moving.cs
+++ Assets/Scripts/Moving.cs
@@ -4,5 +4,4 @@ using System.Collections;
 public interface IMoving
 {
     void moveHandler();
-    void rotateHandler();
 }
--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -1,16 +1,30 @@
-﻿using UnityEngine;
+﻿/*
+ Abstract class for units. Mainly sets up health and damage functions. 
+ Used on any unit, players and NPCs alike.
+ */
+
+using UnityEngine;
 using System.Collections;
 
 public abstract class Unit : MonoBehaviour {
 
+    //All Units have health, and max health.
+    //Max health is for limiting overhealing, and displaying health bars.
     public float health = 1000;
     protected float healthMax;
-
+    
+    //FUNCTION: Start function, initializes variables.
+    //CALLED BY: Unity game engine.
     void Start()
     {
+        //As units are expected to start at full health, maxHealth is initialized to health.
         healthMax = health;
     }
 
+    //FUNCTION: Used to subtract health when unit takes damage. Kills unit if health hits 0.
+    //CALLED BY: Any function wanting to deal damage to the unit.
+    //INPUTS: Amount of damage to deal.
+    //OUTPUTS: Health after damage is dealt.
 	public virtual float TakeDamage (float _damage)
     {
         health -= _damage;
@@ -19,6 +33,10 @@ public abstract class Unit : MonoBehaviour {
         return health;
 	}
 
+    //FUNCTION: Used to add health to unit. Won't go above maximum health.
+    //CALLED BY: Any function wanting that adds health to a unit.
+    //INPUTS: Amount of health to add.
+    //OUTPUTS: New health amount after it's added.
     public virtual float AddHealth (float _addedHealth)
     {
         health += _addedHealth;
@@ -27,7 +45,10 @@ public abstract class Unit : MonoBehaviour {
         return health;
     }
     
-    public void Kill()
+
+    //FUNCTION: Destroys game object.
+    //CALLED BY: TakeDamage, or other functions that would kill the unit. This function should only be called by it's own class.
+    protected void Kill()
     {
         Destroy(gameObject);
     }
