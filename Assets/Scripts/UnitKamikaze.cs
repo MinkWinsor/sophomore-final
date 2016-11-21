d649dfa5fa70ec92a4cf6e4627ccc53364430cc3
MinkWinsor <steven.olsen18@gmail.com>
2016-11-20 18:31:26 -0700

    Starting Recycle Script.

--- Assets/Scripts/UnitKamikaze.cs
+++ Assets/Scripts/UnitKamikaze.cs
@@ -2,5 +2,14 @@
 using System.Collections;
 
 public class UnitKamikaze : Unit {
-    
+
+    public override void Start()
+    {
+
+    }
+
+    public override void Kill()
+    {
+        base.Kill();
+    }
 }
