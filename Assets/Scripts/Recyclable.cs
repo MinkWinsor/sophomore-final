f96985109808bcdd75565eac5f07dc93ee4df0e6
MinkWinsor <steven.olsen18@gmail.com>
2016-11-28 20:43:35 -0700

    Recyclable class is now purely virtual.

--- Assets/Scripts/Recyclable.cs
+++ Assets/Scripts/Recyclable.cs
@@ -1,5 +1,7 @@
 ﻿/*
- 
+ Base class for a Recyclable object. It's important to note that even though a start function example is shown, it should NOT be used. Overriding is required.
+ This is not because the script would not work. it could. But if you use the base class script to collect objects in a list, you will get ALL the Recyclable objects.
+ This includes bullets, enemies, etc.
  */
 
 //Required Libraries
@@ -10,12 +12,12 @@ using System;
 public abstract class Recyclable : MonoBehaviour {
 
     //-Public Variables-//
-    public static Action<Recyclable> RecyclerAction;
+    //public static Action<Recyclable> RecyclerAction; //Each script should have their own version of this.
 
-    //FUNCTION:
-    //CALLED BY:
-    protected virtual void Start () {
-        if (RecyclerAction != null)
+    //FUNCTION: This function should always be overriden. The recyclerAction should be reused with a class-specific name.
+    //CALLED BY: Unity Game Engine
+    protected abstract void Start();
+        /*if (RecyclerAction != null)
         {
 
             RecyclerAction(this);
@@ -23,8 +25,7 @@ public abstract class Recyclable : MonoBehaviour {
         else
         {
             print("RECYCLER NULL");
-        }
-    }
+        }*/
 	
 }
 
